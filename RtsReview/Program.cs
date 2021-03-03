a6b69bb6c849f242c0af9e355069ec089683bef7
Scott Holzwarth <scott@sightsource.net>
2021-03-03 18:36:52 -0500

    pseudo code for exercise one

--- RtsReview/Program.cs
+++ RtsReview/Program.cs
@@ -48,17 +48,34 @@ namespace RtsReview
 
         private static void PerformExerciseOne()
         {
+            Console.WriteLine("You chose Exercise #1 --> Print the number of integers in an array that are above the given input and the number that are below");
+            //prompt user for array
+            //validate array is in correct format 
+                // must start and end with [ ] 
+                // must only contain integers (no strings, decimals, etc.) 
+                // must be comma separated [1, 3, 5]  vs [1 3 5] vs [1-3-5]
+            // edge cases 
+                // only int, no commas to separate on.  ex [1] 
+                // commas in weird places.  ex [,1,,4,]
+                    // -> may do something cool with this where it trys to parse, offers option to accept, otherwise cancels? 
+
+            // if array is valid, prompt for int 
+            // validate int is valid
+
+            // if valid, then loop over array and create 2 new lists 
+            // get count of each list, display to user. 
+            // exit 
 
         }
 
         private static void PerformExerciseTwo()
         {
-
+            Console.WriteLine("You chose Exercise #2 --> ");
         }
 
         private static void PerformExerciseThree()
         {
-
+            Console.WriteLine("You chose Exercise #3 --> ");
         }
     }
 }
